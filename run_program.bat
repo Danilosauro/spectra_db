d45bc1224e683734b89c1c836e00849573017cac
danilosauro <danilodias_lc@hotmail.com>
2024-11-11 19:11:07 -0300

    updates 11-11-24

--- run_program.bat
+++ run_program.bat
@@ -3,13 +3,13 @@ setlocal
 
 where python3 >nul 2>&1
 if %errorlevel% equ 0 (
-    python3 scripts\initial_screen.py
+    python scripts/initial_screen.py
     exit /b %errorlevel%
 )
 
 where python >nul 2>&1
 if %errorlevel% equ 0 (
-    python scripts\initial_screen.py
+    python scripts/initial_screen.py
     exit /b %errorlevel%
 )
 
